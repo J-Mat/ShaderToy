72a75934bcb62ec69fbf84f7e3da928acf138845
J-Mat <zhujieming2003@aliyun.com>
2021-06-29 18:30:56 +0800

    xxx

--- KIFS.glsl
+++ KIFS.glsl
@@ -1,11 +1,26 @@
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 uv = (fragCoord.xy  - 0.5 * iResolution.xy) / iResolution.y;
+    vec2 mouse = iMouse.xy / iResolution.xy;
     uv *= 3.0;    
     vec3 col = vec3(0);
-    uv.x = abs(uv.x);
+
+    float pi = 3.145926;   
+    
+     float angle =  pi * mouse.x;//mouse.x * 3.14159;//../ 2.0; 
+    vec2 n = vec2(sin(angle), cos(angle));
+    //uv.x = abs(uv.x);
+    //uv.x -= 0.5;
+     
+  //dot(uv, n)
+    uv -= n  *  min(dot(uv, n), 0.0) * 2.0;
+    col.rg += uv;
+    
+
     float d = length(uv - vec2(clamp(uv.x, -1.0, 1.0), 0.0));
-    col += smoothstep(0.03, 0.0, d);
-    col.rg += uv; 
+    col += smoothstep(0.03, 0.00, d);
+
+    //col.rg += uv; 
+    
     fragColor = vec4(col, 1.0);
 }
